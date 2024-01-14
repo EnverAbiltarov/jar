bbc950cec96c19f38dda7c39255aea64b8b85880
EnverAbiltarov <abiltarov.e.i.22@gmail.com>
2024-01-14 21:46:10 +0300

    Создание ORM

---
+++ mongooseTasks.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+const Bank = mongoose.model('bank', { name: String });
+
+const bank = new Bank({ name: 'Фляжка' });
+bank.save().then(() => console.log('из титана'));
