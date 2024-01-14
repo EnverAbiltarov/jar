615911330ffbdea23f8ad51c0a2b53a7475712c5
EnverAbiltarov <abiltarov.e.i.22@gmail.com>
2024-01-14 21:59:59 +0300

    Создание схемы

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
+
 const Bank = mongoose.model('bank', { name: String });
+
+
 
 const bank = new Bank({ name: 'Фляжка' });
 bank.save().then(() => console.log('из титана'));
+
+
+
