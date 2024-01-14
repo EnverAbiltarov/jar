31787591a5612e075ea30b15e28669943c809e54
EnverAbiltarov <abiltarov.e.i.22@gmail.com>
2024-01-14 20:56:24 +0300

    Подключение базы данных

---
+++ createDB.js
@@ -0,0 +1,17 @@
+var MongoClient = require('mongodb').MongoClient 
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
+async function run() {
+    try {
+        await client.connect();
+        var database = client.db("jar"); database.dropDatabase()
+        database = client.db("jar");
+        const bank = database.collection("bank");
+        const result = await bank.insertOne({name:"Склянка"}); 
+        console.log(`${result} documents were inserted`);
+    } 
+    finally {
+           await client.close();
+    } 
+}
+run()
